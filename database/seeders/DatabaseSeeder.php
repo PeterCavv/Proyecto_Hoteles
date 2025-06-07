2a6b8a68805786367e42b3482c9ee843afc5bdb3
DESKTOP-O5JI7IM\Peter <pedroabad52@gmail.com>
2025-06-07 11:51:16 +0200

    Añadido nuevo seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             RoomTypeSeeder::class,
             ReviewSeeder::class,
             FeatureSeeder::class,
+            AttractionSeeder::class,
         ]);
     }
 }
